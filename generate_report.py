4671ddc16ec4bbd7e81435412ecb8387633f38de
JusnG29 <juliangartner@gmail.com>
2025-10-09 12:35:11 +0200

    no footer on ToC page + refactoring

--- generate_report.py
+++ generate_report.py
@@ -209,7 +209,8 @@ def get_initial_conditions(case, init_cond_ref, init_cond_mqtg, init_cond_qtg, m
 
 
 # returns structure data and plots for one test.
-def process_test_case_data(test_item, snapshot_data, init_cond_ref, init_cond_mqtg, init_cond_qtg, plot_base64, date_time, mode):
+def process_test_case_data(test_item, snapshot_data, init_cond_ref, init_cond_mqtg, init_cond_qtg, plot_base64,
+                           date_time, mode):
     test_id, part_id, case_id = split_string(test_item['id'])
     test, part, case = get_test_test_part_test_case(qtg_structure['tests'], test_id, part_id, case_id)
 
@@ -217,13 +218,14 @@ def process_test_case_data(test_item, snapshot_data, init_cond_ref, init_cond_mq
     formatted_time = date_time.strftime("%H:%M:%S")
 
     get_initial_conditions(case, init_cond_ref, init_cond_mqtg, init_cond_qtg, mode)
-    is_automatic = False if mode == mode.REFERENCE else test_item['is_automatic']   # references cannot be automatic tests
+    is_automatic = False if mode == mode.REFERENCE else test_item[
+        'is_automatic']  # references cannot be automatic tests
 
     # use 1 page for part, 5 for static case, +1 for snapshot data, ceil(n/3) for plots
     count = 3 if mode != mode.REFERENCE else 2
     if part['snapshot']:
         count += 1
-    count += int(math.ceil(len(plot_base64)/3))
+    count += int(math.ceil(len(plot_base64) / 3))
 
     case.update({
         "is_snapshot": part['snapshot'],
@@ -270,6 +272,7 @@ def process_test_case_na(test_item, date_time, mode):
     }
     return data
 
+
 # Generate the PDF
 # if __name__ == "__main__":
 #     # 1. einen, mehrerer oder alle tests
@@ -299,6 +302,7 @@ def create_graphs_pdf(data, output_file):
     # Convert the rendered HTML to PDF
     HTML(string=html_out).write_pdf(output_file, stylesheets=[CSS(css_path)])
 
+
 # creates a test_case report for one test case with headers.
 def create_test_case_pdf(data, output_dir):
     html_out = populate_template(CASE_WRAPPER_TEMPLATE_NAME, data)
@@ -310,6 +314,7 @@ def create_test_case_pdf(data, output_dir):
     save_document(doc, output_dir)
 
     do_post_processing(doc, word, tmp_file_path)
+
 
 # PUBLIC FUNCTIONS
 
@@ -338,12 +343,14 @@ def create_test_report(test_results, output_dir, mode: TestMode):
 
     doc = open_document(word, tmp_file_path)
 
-    do_footer_table(word, doc, data)
-    do_pagination(word, doc)
     create_table_of_contents(doc)
+    do_pagination(word, doc)
+    do_footer_table(word, doc, data)
+    doc.TablesOfContents(1).Update()
     save_document(doc, output_dir)
 
     do_post_processing(doc, word, tmp_file_path)
+
 
 def do_pagination(word, doc):
     pages = [2, 8]
@@ -366,6 +373,7 @@ def do_pagination(word, doc):
             footer.PageNumbers.RestartNumberingAtSection = True
             footer.PageNumbers.StartingNumber = 1
 
+
 def do_footer_table(word, doc, data):
     doc.Repaginate()
     num_of_pages = doc.ComputeStatistics(2)
@@ -374,7 +382,7 @@ def do_footer_table(word, doc, data):
     pages = [8]  # pages where we move to the next case
     case_index = 0  # track which case we’re on
 
-    for i in range(1, num_of_pages + 1):
+    for i in range(2, num_of_pages + 1):
         if i in pages:
             word.Selection.GoTo(win32.constants.wdGoToPage,
                                 win32.constants.wdGoToAbsolute,
@@ -413,9 +421,11 @@ def do_footer_table(word, doc, data):
         rng.Fields.Add(rng, Type=win32.constants.wdFieldPage)
         rng.ParagraphFormat.Alignment = win32.constants.wdAlignParagraphRight
 
+
 def get_footer(case):
     footer_out = populate_template(CASE_FOOTER_TEMPLATE_NAME, case)
     return create_temp_file(footer_out)
+
 
 def transform_cases(data):
     result = []
@@ -431,6 +441,7 @@ def transform_cases(data):
                 })
     return result
 
+
 def populate_template(file_name, data):
     env = Environment(loader=FileSystemLoader(TEMPLATE_PATH))
     template = env.get_template(file_name)
@@ -443,26 +454,31 @@ def populate_template(file_name, data):
 
     return html_styled
 
+
 def create_temp_file(html_out):
     with tempfile.NamedTemporaryFile("w", suffix=".html", delete=False, encoding="utf-8") as tmp_file:
         tmp_file.write(html_out)
         tmp_file_path = tmp_file.name
     return tmp_file_path
 
+
 def setup_word():
     word = win32.gencache.EnsureDispatch("Word.Application")
-    word.Visible = True # Set to True if you want to see Word opening
+    word.Visible = True  # Set to True if you want to see Word opening
     word.ScreenUpdating = False
     word.DisplayAlerts = False
 
     return word
 
+
 def open_document(word, file_path):
     # GAR: maybe do word.Documents.Open(pdf_path, False, False, False) for supress popups
     return word.Documents.Open(file_path)
 
+
 def save_document(doc, output_dir):
     doc.SaveAs(os.path.join(output_dir, REPORT_FILE_NAME), FileFormat=16)  # 16 = wdFormatDocumentDefault (.docx)
+
 
 # GAR: chat-gpt code -> not used right now but kind of works
 # GAR Notes:   "1 Performance" is Heading 1 no footer
@@ -470,7 +486,6 @@ def save_document(doc, output_dir):
 #               "1.d A1 blabla" is Heading 2 full footer
 # get heading text para.Range.Text
 def page_numbering(doc):
-
     paragraphs = list(doc.Paragraphs)
 
     for idx, para in enumerate(paragraphs):
@@ -497,6 +512,7 @@ def page_numbering(doc):
             footer = section.Footers(win32.constants.wdHeaderFooterPrimary)
             footer.PageNumbers.Add(win32.constants.wdAlignParagraphCenter)
 
+
 # GAR: chat-gpt code works semi
 def footer_stuff(doc):
     for i in range(doc.Tables.Count, 0, -1):
@@ -521,6 +537,7 @@ def footer_stuff(doc):
             # Delete original table in body
             tbl.Delete()
 
+
 def create_table_of_contents(doc):
     toc_range = doc.Range(0, 0)
     doc.TablesOfContents.Add(
@@ -538,10 +555,12 @@ def create_table_of_contents(doc):
     # Insert a page break
     rng.InsertBreak(win32.constants.wdPageBreak)
 
+
 def do_post_processing(doc, word, tmp_file_path):
     doc.Close()
     word.Quit()
     os.remove(tmp_file_path)
+
 
 def generate_case_report(test_item, test_dir, date_time, mode: TestMode):
     if not test_item['is_applicable']:
@@ -553,11 +572,13 @@ def generate_case_report(test_item, test_dir, date_time, mode: TestMode):
     snapshot_data = load_json_snapshots(test_item, test_dir, mode)
     # load existing images
     plots_base64 = load_plots(test_dir, mode)
-    data = process_test_case_data(test_item, snapshot_data, init_cond_ref, init_cond_mqtg, init_cond_qtg, plots_base64, date_time, mode)
+    data = process_test_case_data(test_item, snapshot_data, init_cond_ref, init_cond_mqtg, init_cond_qtg, plots_base64,
+                                  date_time, mode)
     create_test_case_pdf(data, test_dir)
 
     plots_base64 = load_plots(test_dir, mode, only_refer=False)
-    data2 = process_test_case_data(test_item, snapshot_data, init_cond_ref, init_cond_mqtg, init_cond_qtg, plots_base64, date_time, mode)
+    data2 = process_test_case_data(test_item, snapshot_data, init_cond_ref, init_cond_mqtg, init_cond_qtg, plots_base64,
+                                   date_time, mode)
     create_graphs_pdf(data2, os.path.join(test_dir, "Graphs.pdf"))
 
     return data
