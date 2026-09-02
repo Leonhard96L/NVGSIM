756f84bee8b1166262f08b523ce4afe9857a725c
JusnG29 <juliangartner@gmail.com>
2026-09-02 12:36:29 +0200

    typo

--- transport_delay.py
+++ transport_delay.py
@@ -72,7 +72,7 @@ def init_simulation():
     airspeed_x.write(INITIAL_AIRSPEED)
     speed_xy.write(INITIAL_FORWARD_SPEED)
 
-    # TODO GAR check sleeps here. is it necessary. fixed tmie for all sleeps
+    # TODO GAR check sleeps here. is it necessary. fixed time for all sleeps
     simulation_mode.write(SimMode.TRIM)
     time.sleep(2)
 
